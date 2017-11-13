49fcf963863802b5f865d79215cb0c16aed9da6a
slujiani <lujianirely@163.com>
2017-11-13 11:43:36 +0800

    对TestListG类的queryBridgeWords方法的黑盒测试

---
+++ tttest/src/test/java/test/tttest/TestListG3.java
@@ -0,0 +1,20 @@
+package test.tttest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestListG3 {
+	public TestListG G=new TestListG();
+	@Before
+	public void setUp() throws Exception {
+		G=TestListG.createDirectedGraph("test001.txt");
+		TestListG.showDirectedGraph(G);
+	}
+	@Test
+	public void testqueryBridgeWords() {
+		String expected="No word1 or word2 in the graph!";
+		assertEquals(expected,TestListG.queryBridgeWords(G, "too","of"));		
+	}
+}
